e4b6f784e3357e91f71da2739dd3338656c2bdac
Huyuhotaru <108530430+Huyuhortaru@users.noreply.github.com>
2022-11-27 15:03:59 +0000

    [Eleventh]commit

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,14 @@
 class Admin::HomesController < ApplicationController
+  before_action :authenticate_admin!
+  
   def top
+    @order = Order.new
+    @orders = Order.page(params[:page])
   end
-    
+
+  private
+  def order_params
+    params.require(:order).permit(:name, :status)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#管理者用メールアドレスとパスワードをデータベースに登録する記述
 Admin.create!(email: 'test@test', password: 'testpass')
